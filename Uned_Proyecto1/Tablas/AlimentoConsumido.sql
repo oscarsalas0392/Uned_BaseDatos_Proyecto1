e0df1eecdf454f0eaa321b6400b4ed108b7e8996
Oscar <oscar.salas03@gmail.com>
2024-03-10 18:39:18 -0600

    Cambiosde estructura de la dieta

---
+++ Uned_Proyecto1/Tablas/AlimentoConsumido.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[AlimentoConsumido]
+(
+	[Id] INT NOT NULL,
+	[TipoComida] INT NOT NULL,
+	[Dieta] INT NOT NULL,
+	[Descripcion] VARCHAR(100) NOT NULL,
+	
+	CONSTRAINT PK_AlimentoConsumido PRIMARY KEY(Id),
+	CONSTRAINT FK_AlimentoConsumido_Dieta FOREIGN KEY(Dieta) REFERENCES Dieta(Id),
+	CONSTRAINT FK_AlimentoConsumido_TipoComida FOREIGN KEY(TipoComida) REFERENCES TipoComida(Id)
+)
--- Uned_Proyecto1/Tablas/Dieta.sql
+++ Uned_Proyecto1/Tablas/Dieta.sql
@@ -3,13 +3,10 @@
 	[Id] INT NOT NULL IDENTITY(1,1),
 	[Usuario] INT NOT NULL,
 	[Fecha] DATETIME NOT NULL,
-	[TipoComida] INT NOT NULL,
-	[AlimentosConsumidos] VARCHAR(MAX) NOT NULL,
 	[Calorias] INT NOT NULL,
 	[Comentarios] VARCHAR(500),
 
 	CONSTRAINT PK_Dieta PRIMARY KEY(Id),
 	CONSTRAINT FK_Dieta_Usuario FOREIGN KEY(Usuario) REFERENCES Usuario(Id),
-	CONSTRAINT FK_Dieta_TipoComida FOREIGN KEY(TipoComida) REFERENCES TipoComida(Id)
 	
 )
